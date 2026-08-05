a5e7bbdeb0bbf9989fdd5e1842a4820fd192b2b7
MiyanoOsu <nkoctran15235@gmail.com>
2026-08-05 21:52:50 +0700

    Android port

--- source/soulfu.h
+++ source/soulfu.h
@@ -218,7 +218,11 @@ SF_EXTERN unsigned char* kanji_data;
 
 
 // Camera stuff..
+#ifdef __ANDROID__
+#define CAMERA_ROTATION_RATE  45.0f
+#else
 #define CAMERA_ROTATION_RATE  90.0f
+#endif
 #define CAMERA_ZOOM_RATE 0.75f
 #define MIN_CAMERA_DISTANCE 10.0f
 //#define MAX_CAMERA_DISTANCE 50.0f
